9e9f2d1ec39c820582146b0f9bc0651361a6ddc0
ThuraOo <thuraoo98@gmail.com>
2021-12-10 17:16:38 +0630

    tableview animation added

---
+++ CarApp/TableCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableCell.swift
+//  CarApp
+//
+//  Created by ThuraOo on 10/12/2021.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+    
+    @IBOutlet weak var kmLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        kmLbl.layer.borderColor = UIColor(red: 113/255, green: 242/255, blue: 189/255, alpha: 1).cgColor
+    }
+}
--- CarApp/ViewController.swift
+++ CarApp/ViewController.swift
@@ -9,11 +9,100 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableViewContainer: UIView!
+    @IBOutlet weak var tableViewContainerOffsetY: NSLayoutConstraint!
+    @IBOutlet weak var searchResultBarOffsetY: NSLayoutConstraint!
+    @IBOutlet weak var searchBarOffsetY: NSLayoutConstraint!
+    @IBOutlet weak var goBtnWidth: NSLayoutConstraint!
+    @IBOutlet var searchBarOffsetX: NSLayoutConstraint!
+    @IBOutlet weak var searchBarOffsetXEnd: NSLayoutConstraint!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var lblRent: UILabel!
+    @IBOutlet weak var alertBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
+        hideTableContainer(animate: false)
+    }
+    
+    @IBAction func backArrowPressed(_ sender: UIButton) {
+        hideTableContainer(animate: true)
+    }
+    
+    @IBAction func goButtonPressed(_ sender: UIButton) {
+        showTableContainer(animate: true)
+    }
+    
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "TableCell", bundle: nil), forCellReuseIdentifier: "TableCell")
+    }
+    private func prepareUI() {
+        tableViewContainerOffsetY.constant = self.view.bounds.height
+        self.view.layoutIfNeeded()
+    }
+    
+    private func hideTableContainer(animate: Bool) {
+        searchResultBarOffsetY.constant = -120
+        searchBarOffsetY.constant = self.view.bounds.height - 320
+        searchBarOffsetX.constant = 20
+        searchBarOffsetXEnd.constant = 20
+        goBtnWidth.constant = 60
+        tableViewContainerOffsetY.constant = self.view.bounds.height
+        
+        if (animate) {
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+                self.alertBtn.transform = CGAffineTransform(scaleX: 1, y: 1)
+                self.lblRent.layer.opacity = 1
+                self.view.backgroundColor = UIColor.secondarySystemBackground
+                self.view.layoutIfNeeded()
+            }, completion: {res in })
+        } else {
+            self.alertBtn.transform = CGAffineTransform(scaleX: 1, y: 1)
+            self.lblRent.layer.opacity = 1
+            self.view.backgroundColor = UIColor.secondarySystemBackground
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func showTableContainer(animate: Bool) {
+        searchResultBarOffsetY.constant = 0
+        searchBarOffsetY.constant = 70
+        searchBarOffsetX.constant = 0
+        searchBarOffsetXEnd.constant = 0
+        goBtnWidth.constant = 0
+        tableViewContainerOffsetY.constant = 190
+        if (animate) {
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+                self.alertBtn.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+                self.lblRent.layer.opacity = 0
+                self.view.backgroundColor = UIColor.white
+                self.view.layoutIfNeeded()
+            }, completion: {res in })
+        } else {
+            self.alertBtn.transform = CGAffineTransform(scaleX: 0, y: 0)
+            self.lblRent.layer.opacity = 0
+            self.view.backgroundColor = UIColor.white
+            self.view.layoutIfNeeded()
+        }
     }
 
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath)
+        return cell
+    }
+    
+    
 }
 
